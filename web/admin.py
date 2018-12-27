bf3d668a8249c0327082ca146bdde9f1835a9f72
sahellll <abdoli.parsian@gmail.com>
2018-12-27 16:38:47 +0330

    'Income'

--- web/admin.py
+++ web/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
+from .models import Expense , Income
 
 # Register your models here.
-from .models import Expense
+
 
 admin.site.register(Expense)
-
+admin.site.register(Income)
--- web/models.py
+++ web/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.db.models import BigIntegerField
+
 
 class Expense(models.Model):
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
-    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    user = models.ForeignKey(User, on_delete = models.DO_NOTHING)
+    def __unicode___(self):
+        return "()-()".format(self.date, self.amount)
+
+class Income(models.Model):
+    text= models.CharField(max_length=255)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User , on_delete = models.DO_NOTHING)
+    def __unicode___(self):
+        return "()-()".format(self.date, self.amount)
