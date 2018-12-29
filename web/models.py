c04e6bdecfca191f5c461185aa19b1babe960ce1
sahellll <abdoli.parsian@gmail.com>
2018-12-29 15:17:29 +0330

    income

--- web/models.py
+++ web/models.py
@@ -1,5 +1,7 @@
+from __future__ import  unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
+
 
 from django.db.models import BigIntegerField
 
@@ -9,13 +11,13 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete = models.DO_NOTHING)
-    def __unicode___(self):
-        return "()-()".format(self.date, self.amount)
+    def __unicode__(self):
+        return "()-()".format(self.date , self.amount)
 
 class Income(models.Model):
     text= models.CharField(max_length=255)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User , on_delete = models.DO_NOTHING)
-    def __unicode___(self):
-        return "()-()".format(self.date, self.amount)
+    def __unicode__(self):
+        return "()-()".format(self.date , self.amount)
